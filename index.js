9b7e89ee34c933b84aa20a0cf86dc6ba946fa77e
Arnon Hongklay <arnon@hongklay.com>
2017-03-09 13:06:02 +0700

    fix bug

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ this.App || (this.App = {});
 App.cable = ActionCable.createConsumer('ws://api.clubloot.com/cable')
 
 App.announcement = App.cable.subscriptions.create('AnnouncementChannel', {
-  connected: function() {},
+  connected: function() {
+    return this.getAnnouncement("xxx");
+  },
   disconnected: function() {},
   received: function(data) {
-    console.log(data['message']);
+    console.log(data);
+  },
+  getAnnouncement: function(data) {
+    return this.perform('get_announcement', { token: data });
   },
   speak: function(publish, description) {
     return this.perform('speak', {
       publish: publish,
       description: description
     });
-  },
-  getAnnouncement: function(data) {
-    return this.perform('get_announcement', {
-      token: data
-    })
   }
 });
