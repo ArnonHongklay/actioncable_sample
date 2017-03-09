e8a9c95d31e9621aedbe5a7ac8f032a5a716a1cf
Arnon Hongklay <nonmadden@users.noreply.github.com>
2017-03-09 10:28:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // ActionCable = require('actioncable')
 this.App || (this.App = {});
-App.cable = ActionCable.createConsumer('ws://api.clubloot.local:5000/cable')
+App.cable = ActionCable.createConsumer('ws://api.clubloot.com/cable')
 
 App.announcement = cable.subscriptions.create('AnnouncementChannel', {
   connected: function() {},
   disconnected: function() {},
   received: function(data) {
-    // return alert(data['message']);
     console.log(data['message']);
-    // $('.sameheight-item').html(data['message']);
   },
   speak: function(publish, description) {
     return this.perform('speak', {
