6f9408e8598011cfa5846be9f028a4e94d04e82a
Arnon Hongklay <nonmadden@users.noreply.github.com>
2017-03-26 13:55:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,19 @@ App.announcement = App.cable.subscriptions.create('AnnouncementChannel', {
     });
   }
 });
+
+App.contest = App.cable.subscriptions.create("ContestChannel", {
+  connected: function() {},
+  disconnected: function() {},
+  received: function(data) {
+    console.log(data);
+  },
+  // show: function(message) {
+  //   return this.perform(data);
+  // }
+  speak: function(message) {
+    return this.perform('speak', {
+      message: message
+    });
+  }
+});
